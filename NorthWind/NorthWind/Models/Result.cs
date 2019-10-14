af7f1f4ee1830a78f373ce3beff7df832c50a30e
YungJu <lmdmdabear@gmail.com>
2019-10-14 17:38:39 +0800

    add Result Model.

---
+++ NorthWind/NorthWind/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthWind.Models
+{
+    public class Result
+    {
+        public int ResultCode { get; set; } = 200;
+
+        public string ResultMsg { get; set; } = "";
+    }
+}
