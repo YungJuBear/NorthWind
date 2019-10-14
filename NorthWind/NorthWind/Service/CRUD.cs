9e9156c27b735f178d53ecee8676b2174f2eddb4
YungJu <lmdmdabear@gmail.com>
2019-10-14 17:39:05 +0800

    add CRUD Interface.

---
+++ NorthWind/NorthWind/Service/CRUD.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using NorthWind.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace NorthWind.Service
+{
+    public interface ICRUD
+    {
+        Models.Result Create(object Content);
+        Models.Result Update(object Content);
+        Models.Result Delete(dynamic ID);
+        object GetList();
+        Models.Result Save();
+    }
+
+   
+}
