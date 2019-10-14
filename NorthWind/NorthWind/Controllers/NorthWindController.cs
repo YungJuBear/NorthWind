6ccbf8ed91f35b6e8baabbb59f4241629d9fe610
YungJu <lmdmdabear@gmail.com>
2019-10-14 12:11:52 +0800

    add NorthWindController.

---
+++ NorthWind/NorthWind/Controllers/NorthWindController.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NorthWind.Controllers
+{
+    public class NorthWindController : ApiController
+    {
+       // private const string LocalDbMasterConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+
+        //[HttpGet]
+        //public IHttpActionResult Read()
+        //{
+        //    DataTable dataTable = new DataTable();
+
+        //    using (SqlConnection myConn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["NorthWindContext"].ConnectionString))
+        //    {
+               
+
+        //        string sql = @"  SELECT *
+        //                         FROM [Northwind].[dbo].[Customers]";
+        //        using (SqlCommand dataCommand = new SqlCommand(sql, myConn))
+        //        {
+        //            myConn.Open();
+        //            SqlDataReader myDR = dataCommand.ExecuteReader();
+        //            if (myDR.HasRows)
+        //            {
+
+        //                    dataTable.Load(myDR);
+                        
+        //            }
+        //            myDR.Close();
+        //        }
+        //        myConn.Close();
+        //    }
+        //    var Result  = JsonConvert.SerializeObject(dataTable);
+        //    return Ok(new { Result });
+        //}
+
+    }
+}
