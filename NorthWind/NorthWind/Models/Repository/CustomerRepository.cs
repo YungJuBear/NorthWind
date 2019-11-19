4a9095d48860e1cdc16c080e01c598d2105ee4c5
YungJu <lmdmdabear@gmail.com>
2019-11-19 13:47:50 +0800

    Add CustomerRepository.

---
+++ NorthWind/NorthWind/Models/Repository/CustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+using NorthWind.Models;
+using NorthWind.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace NorthWind.Models.Repository
+{
+    public class CustomerRepository : GenericRepository<Customers>,ICustomerRepository
+    {
+        private NorthwindEntities db { get; set; }
+
+        private static ILogger _log = LogManager.GetCurrentClassLogger();
+
+        public Customers GetByID(string id)
+        {
+            return this.Get(x => x.CustomerID == id);
+        }
+
+    }
+}
