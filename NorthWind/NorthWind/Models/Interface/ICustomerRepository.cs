08bd09b14fd4fa213e99da5181e9ae0f7528d2f3
YungJu <lmdmdabear@gmail.com>
2019-11-19 13:47:25 +0800

    Add ICustomerRepository.

---
+++ NorthWind/NorthWind/Models/Interface/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Models.Interface
+{
+    interface ICustomerRepository : ICRUDRepository<Customers>
+    {
+         Customers GetByID (string id);
+    }
+}
